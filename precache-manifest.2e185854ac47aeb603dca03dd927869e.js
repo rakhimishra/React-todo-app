84d14ae5757a7314755bb828291815d7599ab185
rakhimishra <mishrarakhi21@gmail.com>
2020-07-09 21:19:07 +0530

    Updates

---
+++ precache-manifest.2e185854ac47aeb603dca03dd927869e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e9b011b26e06a9a88a39e3cb521ba3",
+    "url": "/React-todo-app/index.html"
+  },
+  {
+    "revision": "f01c32c62693b1461a98",
+    "url": "/React-todo-app/static/css/main.dd35a2e7.chunk.css"
+  },
+  {
+    "revision": "bd0c97b7a831b9122b2d",
+    "url": "/React-todo-app/static/js/2.0512a152.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todo-app/static/js/2.0512a152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f01c32c62693b1461a98",
+    "url": "/React-todo-app/static/js/main.8dc4c3a3.chunk.js"
+  },
+  {
+    "revision": "f77fb2e0c4ef88ebba05",
+    "url": "/React-todo-app/static/js/runtime-main.03684f77.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
